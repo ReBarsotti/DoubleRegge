26699e5303e5fe6076538656b7cf5f786e390242
ReBarsotti <40772859+ReBarsotti@users.noreply.github.com>
2025-11-18 22:40:14 +0100

    removed gamma functions
    removed legacy code from Gamma functions -- some was commented out before. Some was just never called.

--- DoubleReggeLib/DoubleReggeAmp/DblRegge30.cc
+++ DoubleReggeLib/DoubleReggeAmp/DblRegge30.cc
@@ -62,22 +62,11 @@ DblRegge30::calcAmplitude( GDouble** pKin, GDouble* userVars ) const {
 	double alpha1 = m_aPrime*m_aPrime*u[k_t1] + m_a0;
 	double alpha2 = m_aPrime_2*m_aPrime_2*u[k_t]  + m_a0_2;
 
-
-//	complex< double > gam1 = cgamma( -alpha1 );
-//	complex< double > gam2 = cgamma( -alpha2 );
-
 	double eta = m_s0*u[k_s] / (u[k_s2]*u[k_s1]);
 
 
-
-//	complex< double > v1 = (cgamma(alpha1-alpha2)/gam2)*CHGM(-alpha1 , 1-alpha1+alpha2,-1./eta);
-//	complex< double > v2 =( cgamma(alpha2-alpha1)/gam1)*CHGM(-alpha2 , 1-alpha2+alpha1, -1./eta);
-
 	complex< double> v1 = (m_c0 + m_c1*(1./eta) + m_c2*(1./eta)*(1./eta) + m_c3*(1./eta)*(1./eta)*(1./eta));
 	complex< double> v2 = (m_d0 + m_d1*(1./eta) + m_d2*(1./eta)*(1./eta) + m_d3*(1./eta)*(1./eta)*(1./eta));
-
-//	complex< double> v1 = 1 + m_c0*cos(u[k_phiGJ]);
-//	complex< double> v2 = 1 + m_d0*cos(u[k_phiGJ]);
 
 	complex<double> xi1   = ( complex<double>( -1, 0 ) + exp( -i*M_PI*alpha1 ) ) / 2.;
 	complex<double> xi2   = ( complex<double>( -1, 0 ) + exp( -i*M_PI*alpha2 ) ) / 2.;
@@ -95,19 +84,8 @@ DblRegge30::calcAmplitude( GDouble** pKin, GDouble* userVars ) const {
 	// in the intensity -- note the arguments of the exponentials are always
 	// negative and the expreission os contructed to be strictly positive
 
-
-	// careful here!!  if you decide to float a0 or a0Prime in the fit then
-	// these precalculated values cannot be used
-	//complex<double> gam1( u[k_gam1_re], u[k_gam1_im] );
-	//	complex<double> gam2( u[k_gam2_re], u[k_gam2_im] );
-
-	//  these two lines would be appropriate if a0 and/or a0Prime and hence
-	//  the values of alpha are floating in the fit...
-
-
-	//GAMMAS REMOVED
 	return barrier*((ss2*xi1*xi21*v1 + ss1*xi2*xi12*v2 ));
-//	return gam1*gam2*barrier*((ss2*xi1*xi21*v1 + ss1*xi2*xi12*v2 ));
+
 }
 
 void
@@ -133,7 +111,6 @@ DblRegge30::calcUserVars( GDouble** pKin, GDouble* userVars ) const {
 	userVars[k_s1] = ( eta + pi ).M2();
 	userVars[k_t]  = ( eta + pi - beam ).M2();
 	userVars[k_pi] = pi.M();
-	//	userVars[k_tmin] = eta.M2() - 2*eta.E()*beam.E() + 2*eta.Vect().Mag()*beam.Vect().Mag();
 
 	// m_fastParticle is set to 2 for eta and 3 for pi
 	userVars[k_s2] = ( m_fastParticle == 2 ? ( proton + pi ).M2() : ( proton + eta ).M2() );
@@ -172,183 +149,4 @@ DblRegge30::calcUserVars( GDouble** pKin, GDouble* userVars ) const {
 	double alpha1 =  m_aPrime*m_aPrime*userVars[k_t1] + m_a0;
 	double alpha2 =  m_aPrime_2*m_aPrime_2*userVars[k_t]  + m_a0_2;
 
-//	complex< double > gam1 = cgamma( -alpha1 );
-//	complex< double > gam2 = cgamma( -alpha2 );
-
-//	userVars[k_gam1_re] = real( gam1 );
-//	userVars[k_gam1_im] = imag( gam1 );
-
-//	userVars[k_gam2_re] = real( gam2 );
-//	userVars[k_gam2_re] = imag( gam2 );
-}
-
-
-complex<double>
-DblRegge30::cgamma( complex<double> z ) const {
-
-	complex<double> ui (0,1);
-	complex<double> g, infini= 1e308+ 0.0*ui; // z0,z1
-	double x0,q1,q2,x,y,th,th1,th2,g0,gr,gi,gr1,gi1;
-	double na=0.0,t,x1 = 1,y1=0.0,sr,si;
-	int j,k;
-
-
-	static double a[] = {
-		8.333333333333333e-02,
-		-2.777777777777778e-03,
-		7.936507936507937e-04,
-		-5.952380952380952e-04,
-		8.417508417508418e-04,
-		-1.917526917526918e-03,
-		6.410256410256410e-03,
-		-2.955065359477124e-02,
-		1.796443723688307e-01,
-		-1.39243221690590};
-
-	x = real(z);
-	y = imag(z);
-
-	if (x > 171) return infini;
-	if ((y == 0.0) && (x == (int)x) && (x <= 0.0))
-		return infini;
-	else if (x < 0.0) {
-		x1 = x;
-		y1 = y;
-		x = -x;
-		y = -y;
-	}
-	x0 = x;
-	if (x <= 7.0) {
-		na = (int)(7.0-x);
-		x0 = x+na;
-	}
-	q1 = sqrt(x0*x0+y*y);
-	th = atan(y/x0);
-	gr = (x0-0.5)*log(q1)-th*y-x0+0.5*log(2.0*M_PI);
-	gi = th*(x0-0.5)+y*log(q1)-y;
-	for (k=0;k<10;k++){
-		t = pow(q1,-1.0-2.0*k);
-		gr += (a[k]*t*cos((2.0*k+1.0)*th));
-		gi -= (a[k]*t*sin((2.0*k+1.0)*th));
-	}
-	if (x <= 7.0) {
-		gr1 = 0.0;
-		gi1 = 0.0;
-		for (j=0;j<na;j++) {
-			gr1 += (0.5*log((x+j)*(x+j)+y*y));
-			gi1 += atan(y/(x+j));
-		}
-		gr -= gr1;
-		gi -= gi1;
-	}
-
-
-	if (x1 <= 0.0) {
-		q1 = sqrt(x*x+y*y);
-		th1 = atan(y/x);
-		sr = -sin(M_PI*x)*cosh(M_PI*y);
-		si = -cos(M_PI*x)*sinh(M_PI*y);
-		q2 = sqrt(sr*sr+si*si);
-		th2 = atan(si/sr);
-		if (sr < 0.0) th2 += M_PI;
-		gr = log(M_PI/(q1*q2))-gr;
-		gi = -th1-th2-gi;
-		x = x1;
-		y = y1;
-	}
-
-	g0 = exp(gr);
-	gr = g0*cos(gi);
-	gi = g0*sin(gi);
-
-	g = gr + ui*gi;
-
-	return g;
-}
-double DblRegge30::CHGM(double A, double B, double X) const{
-	double A0=A, A1=A, X0=X, HG = 0.0;
-	double TBA, TB, TA, Y0=0.0, Y1=0.0, RG, LA = (int) A, NL, R, M, INF = pow(10,300);
-	double sum1, sum2, R1, R2, HG1, HG2;
-	if (B == 0.0 || B == -abs( (int) B)){
-		HG = INF;
-	} else if(A == 0.0 || X == 0.0) {
-		HG = 1.0;
-	} else if(A == -1.0){
-		HG = 1.0 - X/B;
-	} else if(A == B){
-		HG = exp(X);
-	} else if (A-B == 1.0){
-		HG = (1.0+X/B)*exp(X);
-	} else if (A == 1.0 && B == 2.0){
-		HG = (exp(X)-1.0)/X;
-	} else if(A == (int)A && A < 0.0){
-		M = (int) -A;
-		R = 1.0;
-		HG = 1.0;
-		for (int k = 1; k<= M ; k++) {
-			R = R*(A+k-1.0)/k/(B+k-1.0)*X;
-			HG+=R;
-		}
-	}
-	if(HG != 0){return HG;}
-
-	if(X<0.0){
-		A = B-A;
-		A0 = A;
-		X = fabs(X);
-	}
-	if(A<2.0) {NL = 0;}
-	else{
-		NL = 1;
-		LA = (int) A;
-		A  = A-LA-1.0;
-	}
-	for (int n = 0; n<= NL; n++) {
-		if(A0 >= 2.0 ) { A+=1.0; }
-		if(X <= 30.0 + fabs(B) || A < 0.0){
-			HG = 1.0;
-			RG = 1.0;
-			for (int j = 1; j<= 500; j++) {
-				RG = RG*(A+j-1)/(j*(B+j-1))*X;
-				HG += RG;
-				if(fabs(RG/HG) < pow(10.,-15.)) {
-					if(n==0) {Y0 = HG;}
-					if(n==1) {Y1 = HG;}
-				}
-				continue;
-			}
-		} else {
-			TA = tgamma(A);
-			TB = tgamma(B);
-			TBA = tgamma(B-A);
-			sum1 = 1.0;
-			sum2 = 1.0;
-			R1 = 1.0;
-			R2 = 1.0;
-			for (int i = 1; i<=8; i++) {
-				R1 = - R1*(A+i-1)*(A-B+i)/(X*i);
-				R2 = - R2*(B-A+i-1)*(A-i)/(X*i);
-				sum1+=R1;
-				sum2+=R2;
-			}
-			HG1 = TB/TBA*pow(X,-A)*cos(M_PI*A)*sum1;
-			HG2 = TB/TA*exp(X)*pow(X,A-B)*sum2;
-			HG = HG1+HG2;
-		}
-		if(n==0) {Y0 = HG;}
-		if(n==1) {Y1 = HG;}
-	}
-	if(A0 >= 2.0){
-		for (int i=1; i<=LA-1; i++) {
-			HG = ((2.*A-B+X)*Y1+(B-A)*Y0)/A;
-			Y0 = Y1;
-			Y1 = HG;
-			A += 1.;
-		}
-	}
-	if(X0<0.0) {HG = HG*exp(X0);}
-	A = A1;
-	X = X0;
-
-	return HG;
 }
